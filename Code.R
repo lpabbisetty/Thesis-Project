62df8f9ee194a1a8edd10954a09f64960de95e62
lpabbisetty <lrpab@ucdavis.edu>
2017-06-06 10:02:53 -0700

    Update

---
+++ Code.R
@@ -0,0 +1,17 @@
+---
+  title: "Thesis Project Code"
+output: 
+  html_document: 
+  keep_md: yes
+---
+  
+#trimming and mapping
+#```{r}
+# fastqc
+#trimmomatic
+#fastqc
+#kallisto
+#mv BrapaV2.1PacBio.cds.fa.ft.removed.updated BrapaV2.1PacBio.cds.fa
+#kallisto index -i BrapaV2.1PacBio.cds.kai BrapaV2.1PacBio.cds.fa
+#kallisto quant --single --plaintext -l 250 -s 50 -t 12 -i ~/reference/brapa/BrapaV2.1PacBio.cds.kai -o kallisto_output br_rnaseq_bt_sush_4_7_16e_S8_L004_R1_001.fastq.gz_trimmed2.fastq
+```
